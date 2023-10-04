27b1807dc281dd71fe3d69300e6576a90f0f2d07
Gabriel Soares <gsoaresdesouza@live.com>
2023-10-04 08:43:49 -0300

    Filtrar produtos, COALESCE, List

--- src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.devsuperior.dscatalog.repositories;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscatalog.entities.Category;
@@ -14,8 +15,8 @@ import com.devsuperior.dscatalog.entities.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
   @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
-      + "(:category IS NULL OR cats = :category) AND "
-      + "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')) )")
-  Page<Product> find(@Param("category") Category category, String name, Pageable pageable);
+      + "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
+      + "(:name = '' OR LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))) ")
+  Page<Product> find(List<Category> categories, String name, Pageable pageable);
 
 }
